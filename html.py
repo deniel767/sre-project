ab139dc0ffaf8dcc3bcd9f3641b029618bb2982e
Daniel Budanyi <deniel767@gmail.com>
2024-08-01 13:41:38 +0200

    added_py_file

---
+++ html.py
@@ -0,0 +1,15 @@
+# Creating an HTML file 
+file = open("test.html","w") 
+   
+# Adding input data to the HTML file 
+file.write("<html>")
+file.write("<head>")
+file.write("<title>My Webpage</title>")
+file.write("</head>")
+file.write("<body>")
+file.write("<h1>Welcome to my webpage!</h1>")
+file.write("</body>")
+file.write("</html>")
+              
+# Saving the data into the HTML file 
+file.close()
